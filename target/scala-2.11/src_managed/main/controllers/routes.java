e9bcc8c92f2c02425b9ee0b44eb5970894279eea
Juan Ignacio Sánchez Lara <juanignaciosl@gmail.com>
2014-06-19 07:09:42 +0200

    deployed on jueves, 19 de junio de 2014, 07:08:52 CEST

--- target/scala-2.11/src_managed/main/controllers/routes.java
+++ target/scala-2.11/src_managed/main/controllers/routes.java
@@ -1,6 +1,6 @@
 // @SOURCE:/Users/juanignaciosl/Development/workspaceKepler/wedding-tables-planner-site/conf/routes
 // @HASH:05e5043d208d83f2538c39224e2668c24c77d7e0
-// @DATE:Wed Jun 18 20:18:05 CEST 2014
+// @DATE:Thu Jun 19 07:09:03 CEST 2014
 
 package controllers;
 
